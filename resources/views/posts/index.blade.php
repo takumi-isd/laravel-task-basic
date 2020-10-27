e979a999b6370ae18969a86772a68f1244cc0c10
EC2 Default User <ec2-user@ip-172-31-1-194.ap-northeast-1.compute.internal>
2020-10-27 05:52:39 +0000

    Laravel基礎課題4の回答

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 
 <div class="container mt-4">
+    @foreach($posts as $post)
     <div class="card mb-4">
         <div class="card-header mb-2">
-            Title
+            {{ $post->title }}
         </div>
         <div class="card-body">
             <p class="card-text">
-                body
+                {{ $post->body }}
             </p>
         </div>
         <div class="card-footer">
@@ -18,6 +19,7 @@
             </span>
         </div>
     </div>
+    @endforeach
 </div>
 
 @endsection('content')
